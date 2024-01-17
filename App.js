dd7ad7bbe66a194873a0388675320ccc613ada3a
Mass Jack <mass.jack4@gmail.com>
2024-01-17 08:28:03 +0100

    Add background color on the root screen

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <StartGameScreen/>
+      <View style={styles.rootScreen}>
+        <StartGameScreen/>
+      </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-
+  rootScreen: {
+    flex: 1,
+    backgroundColor: '#F0E1FF',
+  }
 
 });
