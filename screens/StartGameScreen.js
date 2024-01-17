611d1a2f449bf4538d116fdea861e305032a3626
Mass Jack <mass.jack4@gmail.com>
2024-01-17 08:19:14 +0100

    Improve buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,8 +5,14 @@ function StartGameScreen() {
     return (
             <View style={styles.inputContainer}>
                 <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false} />
-                <PrimaryButton>Reset</PrimaryButton>
-                <PrimaryButton>Confirm</PrimaryButton>
+                <View style={styles.buttonsContainer}>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton>Reset</PrimaryButton>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton>Confirm</PrimaryButton>
+                    </View>
+                </View>
             </View>
         )
 
@@ -16,6 +22,8 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
     inputContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
         marginTop: 100,
         marginHorizontal: 24,
         padding: 16,
@@ -41,5 +49,11 @@ const styles = StyleSheet.create({
         shadowRadius: 6,
         shadowOpacity: 0.25,
         textAlign: 'center',
-    }
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+    },
+    buttonContainer: {
+        flex: 1,
+    },
 });
