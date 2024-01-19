f75f61883d5be7b546b065fe62c97f8ab8dc19ef
Mass Jack <mass.jack4@gmail.com>
2024-01-19 16:44:29 +0100

    Feat: Add handling user input & show alert

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,9 +1,6 @@
 import { View, Text, Pressable, StyleSheet } from 'react-native';
 
-function PrimaryButton({ children }) {
-    function pressHandler() {
-        console.log('Pressed!');
-    }
+function PrimaryButton({ children, onPress }) {
 
     return (
         <View style={styles.buttonOuterContainer}>
@@ -13,7 +10,7 @@ function PrimaryButton({ children }) {
                         ? [styles.buttonInnerContainer, styles.pressed]
                         : styles.buttonInnerContainer
                 }
-                onPress={pressHandler}
+                onPress={onPress}
                 android_ripple={{ color: '#8B51D8' }}
             >
                 <Text style={styles.buttonText}>{children}</Text>
