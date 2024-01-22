5e1a4a97b2e22649ae69a4890630d5c0f90c170d
Mass Jack <mass.jack4@gmail.com>
2024-01-22 07:57:05 +0100

    Feat: Log game data

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { View, StyleSheet, Alert } from 'react-native';
+import { View, StyleSheet, Alert, Text, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import NumberContainer from '../components/game/NumberContainer';
 import PrimaryButton from '../components/ui/PrimaryButton';
@@ -27,6 +27,7 @@ function GameScreen({ userNumber, onGameOver }) {
         userNumber
     );
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
+    const [guessRounds, setGuessRounds] = useState([initialGuess]);
 
     useEffect(() => {
         if (currentGuess === userNumber) {
@@ -64,6 +65,7 @@ function GameScreen({ userNumber, onGameOver }) {
             currentGuess
         );
         setCurrentGuess(newRndNumber);
+        setGuessRounds(prevGuessRounds => [newRndNumber, ...prevGuessRounds]);
     }
 
     return (
@@ -86,7 +88,10 @@ function GameScreen({ userNumber, onGameOver }) {
 
                 </View>
             </Card>
-            {/* <View>LOG ROUNDS</View> */}
+            <View>
+                {/* {guessRounds.map(guessRound => <Text key={guessRound}>{guessRound}</Text>)} */}
+                <FlatList data={guessRounds} renderItem={(itemData) => <Text>{itemData.item}</Text>} keyExtractor={(item) => item } />
+            </View>
         </View>
     );
 }
